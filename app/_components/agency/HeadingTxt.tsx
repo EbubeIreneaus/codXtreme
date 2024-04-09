a37a2f1b51a1fe4842db4db4233d9d9463bc8afc
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-04-09 09:53:51 +0100

    our work design completed

--- app/_components/agency/HeadingTxt.tsx
+++ app/_components/agency/HeadingTxt.tsx
@@ -4,7 +4,7 @@ export default function  HeadingTxt({text}: {text: string}){
 
     return (
         <div>
-            <span className=" w-40 py-1 bg-orange-500 mb-5 block"></span>
+            <span className=" w-24 py-1 bg-orange-500 mb-5 block"></span>
             <h2 className="text-4xl font-bold text-white">{text}</h2>
         </div>
     )
--- app/_components/agency/LatestWork.tsx
+++ app/_components/agency/LatestWork.tsx
@@ -23,19 +23,24 @@ function LatestWork() {
   ];
 
   return (
-    <div className="py-28 bg-black/50">
-      <div className="mb-14 px-10 ">
-        <HeadingTxt text="Latext Work" />
+    <div className="py-32 bg-black/50">
+      <div className="mb-16 px-10 ">
+        <HeadingTxt text="Our Work" />
       </div>
 
-      <ul className="flex flex-wrap mx-auto px-3">
+      <ul className="flex flex-wrap mx-auto px-3 gap-3">
         {works.map((work, index) => (
-          <li key={work.title} className={`inline shadow-md p-3`}>
+          <li key={work.title} className="inline shadow-md  relative hover:-mt-5 transition-all ease-in-out duration-300">
             <img
               src={`/img/work/${work.src}`}
               alt={work.title.toLowerCase()}
               className="object-cover max-h-[400px]"
             />
+           <div className="py-2 w-full absolute z-20 bg-orange-600/80 bottom-0 left-0 text-center font-semibold">
+            <p>
+              {work.title}
+            </p>
+           </div>
           </li>
         ))}
       </ul>
