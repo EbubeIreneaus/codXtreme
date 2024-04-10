a3986e86ec64b64575ac058e28c6cbb643e3072d
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-04-10 20:41:24 +0100

    errorpage and notfound page component changed to min-h-screen instead of h-screen

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ import { Button } from "primereact/button";
 
 function NotFound() {
   return (
-    <div className="h-screen w-screen bg-slate-950">
+    <div className="min-h-screen w-screen bg-slate-950">
       <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
         <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
           <div className="relative">
